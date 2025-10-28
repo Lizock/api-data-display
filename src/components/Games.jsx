ae5d5715308f056b4c05b5d7b945e521bed441f4
EmilyM <155667566+EmilyMenken@user.noreply.github.com>
2025-10-28 14:53:41 -0700

    style changes

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -2,13 +2,20 @@ export default function Games({ games }) {
   const uniqueGames = [...new Set(games.map(g => g.version.name))];
 
   return (
+    <div>
+    <div className="gamesTitle">
+    <h4> Pikachu appears in these games:</h4>
+    </div>
     <div className = "Games">
-      <h4> Pikachu appears in these games:</h4>
       <ul>
         {uniqueGames.map((name, i) => (
           <li key={i}>{name}</li>
         ))}
       </ul>
-    </div>
+  </div>
+
+  </div>
+      
+    
   )
 }
